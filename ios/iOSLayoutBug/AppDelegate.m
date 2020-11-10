66f12482407e8d9b64a1e4786e2f48f00c5809b7
Alex Moller <amloelxer@gmail.com>
2020-11-10 17:51:14 -0500

    put back the old app delegate stuff

--- ios/iOSLayoutBug/AppDelegate.m
+++ ios/iOSLayoutBug/AppDelegate.m
@@ -3,7 +3,6 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "TestViewController.h"
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -39,10 +38,9 @@ static void InitializeFlipper(UIApplication *application) {
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-//  UIViewController *rootViewController = [UIViewController new];
-//  rootViewController.view = rootView;
-  TestViewController *myVC = [TestViewController new];
-  self.window.rootViewController = myVC;
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
 }
